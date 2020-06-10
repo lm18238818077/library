ce35a46fbb2bb0860531997c975835923787d793
liming <liming@dfwsgroup.com>
2020-06-10 18:49:46 +0800

    outputPath

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   title: 'lib',
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'dist',
+  outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
 });
